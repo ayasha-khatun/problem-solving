9f0d0d6826e8dc51e76e86d879c7e0d0a6ead284
ayasha-khatun <ayashakhatun004@gmail.com>
2026-06-11 16:26:08 +0600

    first commit

---
+++ Day-4/problems.js
@@ -0,0 +1,13 @@
+// count object properties
+function countProperties(obj){
+    return Object.keys(obj).length;
+}
+
+
+const result = { a:1, b:2, c:3 };
+console.log(result);
+
+// merge two object
+
+
+
