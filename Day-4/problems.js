9a3d54b6e629b7cf2f8f2f19be533caeb740033f
ayasha-khatun <ayashakhatun004@gmail.com>
2026-06-12 20:14:03 +0600

    solve merge two object funtion

--- Day-4/problems.js
+++ Day-4/problems.js
@@ -8,6 +8,10 @@ const result = { a:1, b:2, c:3 };
 console.log(result);
 
 // merge two object
+function mergeObjects(obj1, obj2){
+    return {...obj1, ...obj2};
+}
 
+console.log(mergeObjects({a:1}, {b:2}));
 
 
