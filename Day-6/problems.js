dad7ae69709881e7e1a118fd9204ab5dcae193c6
ayasha-khatun <ayashakhatun004@gmail.com>
2026-06-14 16:36:48 +0600

    solve promise chain function

--- Day-6/problems.js
+++ Day-6/problems.js
@@ -20,3 +20,18 @@ const b = [3, 4];
 const c = [5];
 
 console.log(mergeArrays(a, b, c));
+
+// Promise Chain
+
+function delay(ms){
+    return new Promise((resolve) =>
+    setTimeout(resolve, ms));
+}
+
+delay(1000)
+    .then(() => { 
+        console.log('1 sec'); 
+        return delay(2000); 
+    })
+    .then(() => 
+        console.log('3 sec total'));
